3fff6120a29ff88129787b6abfaa1c378ed9f759
Celina Alzenor <you@example.com>
2021-11-15 14:42:30 -0500

    created fromCSV method (reads in CSV file and displays in table) user can now save and load files

--- src/main/java/ucf/assignments/Actions.java
+++ src/main/java/ucf/assignments/Actions.java
@@ -7,13 +7,18 @@ package ucf.assignments;
 
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
 import java.io.*;
 
 public class Actions {
 
-    public static void removeTask(TableView<Task> tableView, ObservableList<Task> compItems, ObservableList<Task> incItems, ObservableList<Task> toDos) {
-        Task select = tableView.getSelectionModel().getSelectedItem();
+    //removes the selected task
+    public static void removeTask(Task select, TableView<Task> tableView, ObservableList<Task> compItems, ObservableList<Task> incItems, ObservableList<Task> toDos) {
+        //get the selected task
+
+        //remove from table and all lists
         tableView.getItems().remove(select);
         compItems.remove(select);
         incItems.remove(select);
@@ -30,17 +35,13 @@ public class Actions {
 
     }
 
-
-    public static void toCSV(TableView<Task> tableView,String fileName) throws IOException {
+    public static void toCSV(TableView<Task> tableView, File file) throws IOException {
 
         Writer writer = null;
         try {
-            File file = new File("ucf/assignments/"+fileName+".csv.");
             writer = new BufferedWriter(new FileWriter(file));
             for (Task e : tableView.getItems()) {
-
-                String text = e.getDescription() + "," + e.getDueDate() + "," + e.getSelected() + "\n";
-
+                String text = e.getDescription() + "," + e.getDueDate() + "," + e.isComplete() + "\n";
                 writer.write(text);
             }
         } catch (Exception ex) {
@@ -51,9 +52,90 @@ public class Actions {
             writer.flush();
             writer.close();
         }
+    }
+
+    public static void fromCSV(TableView<Task> tableView, File file, ObservableList<Task> comp, ObservableList<Task> inc, ObservableList<Task> all) throws IOException {
+        String splitter = ",", line;
+        String descr = "", date = "";
+        Boolean done = null;
+        String[] reader = null;
+        BufferedReader read = new BufferedReader(new FileReader(file));
+
+        //read data from file
+        while ((line = read.readLine()) != null) {
+            reader = line.split(splitter);
+        }
+
+        String[] info = new String[0];
+        if (reader != null) {
+            info = new String[reader.length];
+            System.arraycopy(reader, 0, info, 0, reader.length);
+        }
+
+        for (String e : info) {
+
+            if (Validation.isDescrValid(e)) {
+                descr = e;
+            } else if (e.equals("false") || e.equals("true")) {
+                done = !e.matches("false");
+            } else {
+                date = e;
+            }
+
+            //if all the information is filled out
+            if (!descr.isEmpty() && !date.isEmpty() && done != null) {
+                Task action = new Task(descr, date, done);
+
+                //if the task isn't either list
+                if (!comp.contains(action) && !inc.contains(action)) {
+                    if (Boolean.TRUE.equals(done)) {
+                        comp.add(action);
+                    } else {
+                        inc.add(action);
+                    }
+                }
+            }
+        }
+
+        //add both lists to the all list
+        all.addAll(inc);
+        all.addAll(comp);
+
+        //clear the current list
+        tableView.getItems().clear();
+        //display all items in the list
+        tableView.setItems(all);
+    }
+
+    public static void save(Window saveStage, FileChooser saver, TableView<Task> table) {
+        saver.setTitle("Save Dialog");
+        saver.setInitialFileName("myToDoList");
+        saver.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("csv file", "*.csv"));
+
+        try {
+            File file = saver.showSaveDialog(saveStage);
+            toCSV(table, file);
+
+            saver.setInitialDirectory(file.getParentFile());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void open(Window openStage, FileChooser saver, TableView<Task> table, ObservableList<Task> comp, ObservableList<Task> inc, ObservableList<Task> all) {
+        saver.setTitle("Load Dialog");
+        saver.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("csv file", "*.csv"));
+
+        try {
+            File file = saver.showOpenDialog(openStage);
+            fromCSV(table, file, comp, inc, all);
+            saver.setInitialDirectory(file.getParentFile());
 
 
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
