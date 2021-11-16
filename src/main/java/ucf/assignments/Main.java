d008799068df31d80127f1a2d30fbd00f6926409
Celina Alzenor <you@example.com>
2021-11-15 22:39:40 -0500

    moved the main method

--- src/main/java/ucf/assignments/Main.java
+++ src/main/java/ucf/assignments/Main.java
@@ -16,6 +16,10 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage window) throws IOException {
 
@@ -28,8 +32,6 @@ public class Main extends Application {
         window.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 
 }
